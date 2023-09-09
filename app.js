4c86a214c793338aef32c11284121f441d7e780d
esconer <hikigaya07@proton.me>
2023-09-10 00:43:33 +0530

    loading message added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const searchBtn = document.querySelector("#search");
-const resultSection = document.querySelector("#result-section")
+const resultSection = document.querySelector("#result-section");
+const waitSection = document.querySelector("#wait");
 
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
@@ -14,30 +15,29 @@ searchBtn.addEventListener("click", function (event) {
     ep_no: Number(epNo),
     lang: lang,
   };
+  waitSection.classList.remove("hidden");
   fetch("http://127.0.0.1:3000/search", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(aniDetails),
-   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.text(); // Parse the response as plain text
   })
-  .then(text => {
-  resultSection.classList.remove('hidden')
-  const ulElement = document.createElement('ul')
-  const liElement = document.createElement('li')
-  
-  liElement.innerHTML= `<a  href=${text}
-  target="_blank"> ${aniDetails.ani_name} episode ${aniDetails.ep_no} ${aniDetails.lang}</a>`
-  ulElement.appendChild(liElement)
-  resultSection.children[0].appendChild(ulElement)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text(); // Parse the response as plain text
+    })
+    .then((text) => {
+      waitSection.classList.add("hidden");
+      resultSection.classList.remove("hidden");
+      const ulElement = document.createElement("ul");
+      const liElement = document.createElement("li");
 
-  })
-
-
+      liElement.innerHTML = `<a  href=${text}
+  target="_blank"> ${aniDetails.ani_name} episode ${aniDetails.ep_no} ${aniDetails.lang}</a>`;
+      ulElement.appendChild(liElement);
+      resultSection.children[0].appendChild(ulElement);
+    });
 });
