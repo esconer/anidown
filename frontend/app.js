ef9baba98ad0e8a2b8af8eaeace2d16e1c8b9260
esconer <hikigaya07@protonmail.com>
2023-09-30 11:49:37 +0530

    pp fix 2

--- frontend/app.js
+++ frontend/app.js
@@ -19,7 +19,7 @@ searchBtn.addEventListener("click", function (event) {
   waitSection.classList.add("hidden");
   errorSection.classList.add("hidden");
   waitSection.classList.remove("hidden");
-  fetch("http://127.0.0.1:3000/search", {
+  fetch("https://anidown-backend.onrender.com/search", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
